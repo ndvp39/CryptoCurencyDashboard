20bf6c727b7d01e083d3c7e937bb35f3e5dc6be2
ndvp39 <ndvp39@gmail.com>
2024-02-06 12:00:39 +0200

    added tabs

--- src/script.js
+++ src/script.js
@@ -6,10 +6,21 @@
         }
 
         // view
-        const setView = (v) => {
+        const setView = (v, tabId) => {
             document.querySelector('h1').innerText = v
             var randomColor = Math.floor(Math.random()*16777215).toString(16)
             document.querySelector('h1').style.borderBottom = `10px solid #${randomColor}`
+
+            // show only the tab that was clicked
+            var tabContents = document.getElementsByClassName('tab-content');
+            Array.from(tabContents).forEach((el) => {
+                el.classList.add('hidden');
+            })
+            document.getElementById(tabId).classList.remove('hidden');
+
+
+            
+
             toggleMenu(true)
         }
 
